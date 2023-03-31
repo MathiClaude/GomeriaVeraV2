e6dc8cedc0c8e04c02f1f7718c87e9cf5aea49a3
MathiClaude <mathivillasantin@gmail.com>
2023-03-30 23:06:29 -0400

    Validacion para el cierre de sesion

--- src/main/java/com/proyecto/is2/proyecto/security/SecurityConfiguration.java
+++ src/main/java/com/proyecto/is2/proyecto/security/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login")//mensaje de logout
+                .logoutSuccessUrl("/login?logout")//mensaje de logout
                 .permitAll();
     }
 }

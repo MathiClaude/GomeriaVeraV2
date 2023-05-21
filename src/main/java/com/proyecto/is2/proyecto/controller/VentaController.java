8d2d340679196ca263104f28b4f8146639c896a2
Gianni <gianniarami@gmail.com>
2023-05-21 17:39:18 -0400

    módulo de venta

--- src/main/java/com/proyecto/is2/proyecto/controller/VentaController.java
+++ src/main/java/com/proyecto/is2/proyecto/controller/VentaController.java
@@ -2,6 +2,7 @@ package com.proyecto.is2.proyecto.controller;
 import com.proyecto.is2.proyecto.controller.dto.UsuarioDTO;
 import com.proyecto.is2.proyecto.model.Rol;
 import com.proyecto.is2.proyecto.model.Usuario;
+import com.proyecto.is2.proyecto.model.Servicio;
 import com.proyecto.is2.proyecto.services.RolServiceImp;
 import com.proyecto.is2.proyecto.services.UsuarioServiceImp;
 import com.proyecto.is2.proyecto.services.ProductoServiceImp;
@@ -65,6 +66,8 @@ public class VentaController {
         boolean crear = usuarioService.tienePermiso("crear-" + VIEW);
         boolean eliminar = usuarioService.tienePermiso("eliminar-" + VIEW);
         boolean actualizar = usuarioService.tienePermiso("actualizar-" + VIEW);
+        boolean seleccionar = usuarioService.tienePermiso("seleccionar-" + VIEW);
+
 
 
         if(consultar) {
@@ -79,6 +82,8 @@ public class VentaController {
         model.addAttribute("permisoCrear", crear);
         model.addAttribute("permisoEliminar", eliminar);
         model.addAttribute("permisoActualizar", actualizar);
+        model.addAttribute("permisoSeleccionar", seleccionar);
+    
 
         return FORM_VIEW;
     }
@@ -210,34 +215,5 @@ public class VentaController {
             return RD_FALTA_PERMISO_VIEW;
         }
     }
-
-//    @GetMapping("asignar-rol")
-//    public String asignarRolUsuario() {
-//        this.operacion = "asignar-rol-";
-//
-//        if(usuarioService.tienePermiso(operacion + VIEW)) {
-//            if(usuarioService.tienePermiso(operacion + VIEW)) {
-//                return ASIGNAR_ROL_VIEW;
-//            }
-//        }
-//        return FALTA_PERMISO_VIEW;
-//    }
-
-//    @PostMapping("asignar-rol")
-//    public String asignarRolUsuarioForm(@RequestParam("id_rol") Long idRol, @RequestParam("username") String email) {
-//        this.operacion = "asignar-rol-";
-//
-//        if(usuarioService.tienePermiso(operacion + VIEW)) {
-//            Usuario usuario = usuarioService.existeUsuario(email);
-//            Rol rol = rolService.existeRol(idRol);
-//
-//            if(usuario != null && rol != null) {
-//                usuario.setRol(rol);
-//                usuarioService.guardar(usuario);
-//                return RD_ASIGNAR_ROL_VIEW;
-//            }
-//        }
-//        return RD_FALTA_PERMISO_VIEW;
-//    }
 
 }
